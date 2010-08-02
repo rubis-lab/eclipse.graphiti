a12299eb421147a8094f206b000c5333c02dfce5
jpasch <jpasch>
2010-08-02 13:09:55 +0000

    add gradient type

---
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/util/IGradientType.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * <copyright>
+ *
+ * Copyright (c) 2005, 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API, implementation and documentation
+ *
+ * </copyright>
+ *
+ *******************************************************************************/
+package org.eclipse.graphiti.util;
+
+public interface IGradientType {
+	
+	public final int VERTICAL = 0;
+	
+	public final int HORIZONTAL = 1;
+
+}
