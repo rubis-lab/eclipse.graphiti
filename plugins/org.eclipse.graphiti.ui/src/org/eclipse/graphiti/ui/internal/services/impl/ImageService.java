0ba282a6332cddf82f36c71bdd019f57e4633088
mgorning <mgorning>
2010-08-06 08:46:41 +0000

    implementation of image service changed / image pool deleted

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/services/impl/ImageService.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/services/impl/ImageService.java
@@ -15,10 +15,15 @@
  *******************************************************************************/
 package org.eclipse.graphiti.ui.internal.services.impl;
 
-import org.eclipse.graphiti.ui.internal.ImagePool;
+import org.eclipse.graphiti.ui.internal.GraphitiUIPlugin;
+import org.eclipse.graphiti.ui.internal.platform.ExtensionManager;
+import org.eclipse.graphiti.ui.platform.IImageProvider;
 import org.eclipse.graphiti.ui.services.IImageService;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
  * @noinstantiate This class is not intended to be instantiated by clients.
@@ -28,11 +33,89 @@ public class ImageService implements IImageService {
 
 	@Override
 	public ImageDescriptor getImageDescriptorForId(String imageId) {
-		return ImagePool.getImageDescriptorForId(imageId);
+
+		if (imageId == null)
+			return null;
+
+		ImageRegistry imageRegistry = GraphitiUIPlugin.getDefault().getImageRegistry();
+		ImageDescriptor imageDescriptor = imageRegistry.getDescriptor(imageId);
+		if (imageDescriptor != null) {
+			return imageDescriptor;
+		}
+
+		imageDescriptor = createImageDescriptorForId(imageId);
+		return imageDescriptor;
 	}
 
 	@Override
 	public Image getImageForId(String imageId) {
-		return ImagePool.getImageForId(imageId);
+
+		if (imageId == null)
+			return null;
+
+		// if image already available take it
+		ImageRegistry imageRegistry = GraphitiUIPlugin.getDefault().getImageRegistry();
+		Image result = imageRegistry.get(imageId);
+		if (result != null)
+			return result;
+
+		createImageDescriptorForId(imageId);
+		return imageRegistry.get(imageId); // now there is an image registered
+	}
+
+	/**
+	 * This method fixes a problem for ImageDescriptors. It returns a corrected
+	 * ImageDescriptor for problematic ImageDescriptors.
+	 * <p>
+	 * There is a problem with transparent GIFs. If the RGB value of the
+	 * transparent color index is identical to the RGB value of other colors
+	 * indices in the palette, then all those color indices are considered as
+	 * transparent. So as a result the transparency seems to be on an RGB value
+	 * instead of a color index.
+	 */
+	private ImageDescriptor fixImageDescriptor(ImageDescriptor descriptor) {
+		// Typically the incoming ImageDescriptor is an URLImageDescriptor. The following lines create an ImageDataImageDescriptor
+		// from it, which basically describes the same image. But that one works. So there seems to be an error in the
+		// URLImageDescriptor.
+		ImageData data = descriptor.getImageData();
+		return ImageDescriptor.createFromImageData(data);
+
+	}
+
+	private ImageDescriptor createImageDescriptorForId(String imageId) {
+
+		if (imageId == null)
+			return null;
+
+		// if image descriptor already exists return it
+		ImageRegistry imageRegistry = GraphitiUIPlugin.getDefault().getImageRegistry();
+		ImageDescriptor imageDescriptor = imageRegistry.getDescriptor(imageId);
+		if (imageDescriptor != null) {
+			return imageDescriptor;
+		}
+
+		// try get the image-location from the image-providers
+		IImageProvider imageProviders[] = ExtensionManager.getSingleton().getImageProviders();
+		for (int i = 0; i < imageProviders.length; i++) {
+			IImageProvider imageProvider = imageProviders[i];
+			String imageFilePath = imageProvider.getImageFilePath(imageId);
+			if (imageFilePath != null) {
+				String pluginId = imageProvider.getPluginId();
+				if (pluginId != null) {
+					// try to create Image from ImageDescriptor (initialize the ImageRegistry on the fly)
+					imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(pluginId, imageFilePath);
+				}
+				break;
+			}
+		}
+
+		if (imageDescriptor == null) {
+			imageDescriptor = ImageDescriptor.getMissingImageDescriptor();
+		}
+
+		imageDescriptor = fixImageDescriptor(imageDescriptor);
+		imageRegistry.put(imageId, imageDescriptor);
+
+		return imageDescriptor;
 	}
 }
