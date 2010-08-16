061c8cdcb79caa1bbab4112ac6daffcbf585d759
jpasch <jpasch>
2010-08-16 12:21:47 +0000

    Clean-up, remove non-javaDoc

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/dt/AbstractDiagramTypeProvider.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/dt/AbstractDiagramTypeProvider.java
@@ -51,48 +51,13 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 		super();
 	}
 
-	/*
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#createLink(java.lang.Object
-	 * [])
-	 */
-	// public void link(PictogramElement pictogramElement, Object[]
-	// businessObjects) {
-	// // create link
-	// PictogramLink newLink =
-	// PackageUtil.getLinksPackage(getDiagram()).getPictogramLink().createPictogramLink();
-	// newLink.setPictogramElement(pictogramElement);
-	//
-	// for (int i = 0; i < businessObjects.length; i++) {
-	// List linkBos = newLink.getBusinessObjects();
-	// linkBos.add(businessObjects[i]);
-	// }
-	//
-	// // add new link to diagram-link
-	// if (getDiagramLink() != null) {
-	// getDiagramLink().getPictogramLinks().add(newLink);
-	// }
-	// }
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.graphiti.dt.IDiagramTypeProvider#
-	 * getAvailableToolBehaviorProviders ()
-	 */
 	public IToolBehaviorProvider[] getAvailableToolBehaviorProviders() {
-		if (availableToolBehaviorProviders == null) {
-			availableToolBehaviorProviders = new IToolBehaviorProvider[] { new DefaultToolBehaviorProvider(this) };
+		if (this.availableToolBehaviorProviders == null) {
+			this.availableToolBehaviorProviders = new IToolBehaviorProvider[] { new DefaultToolBehaviorProvider(this) };
 		}
-		return availableToolBehaviorProviders;
+		return this.availableToolBehaviorProviders;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#getCurrentToolBehaviorProvider
-	 * ()
-	 */
 	public IToolBehaviorProvider getCurrentToolBehaviorProvider() {
 		IToolBehaviorProvider ret = null;
 		if (getAvailableToolBehaviorProviders().length > 0) {
@@ -102,15 +67,15 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 	}
 
 	public int getCurrentToolBahaviorIndex() {
-		return currentToolBahaviorIndex;
+		return this.currentToolBahaviorIndex;
 	}
 
 	public void setCurrentToolBahaviorIndex(int index) {
-		if (currentToolBahaviorIndex != index) {
+		if (this.currentToolBahaviorIndex != index) {
 			if (index < 0 || index >= getAvailableToolBehaviorProviders().length) {
 				throw new IllegalArgumentException("Index not valid"); //$NON-NLS-1$
 			}
-			currentToolBahaviorIndex = index;
+			this.currentToolBahaviorIndex = index;
 
 			IDiagramEditor de = getDiagramEditor();
 			de.refresh();
@@ -118,20 +83,10 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#getDiagram()
-	 */
 	public Diagram getDiagram() {
-		return diagram;
+		return this.diagram;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#getDiagramTitle()
-	 */
 	public String getDiagramTitle() {
 		String name = ""; //$NON-NLS-1$
 		if (getDiagram() != null) {
@@ -140,43 +95,17 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 		return name;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#getDiagramEditor()
-	 */
 	public IDiagramEditor getDiagramEditor() {
-		return diagramEditor;
+		return this.diagramEditor;
 	}
 
-	/*
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#getActionProvider()
-	 */
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.features.IFeatureProviderHolder#getFeatureProvider()
-	 */
 	public IFeatureProvider getFeatureProvider() {
-		if (featureProvider == null) {
+		if (this.featureProvider == null) {
 			T.racer().error("featureProvider is null"); //$NON-NLS-1$
 		}
-		return featureProvider;
+		return this.featureProvider;
 	}
 
-	/*
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#init(org.eclipse.graphiti.mm
-	 * .pictograms.Diagram, org.eclipse.graphiti.platform.IPlatformService)
-	 */
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#init(org.eclipse.graphiti.mm
-	 * .pictograms.Diagram, org.eclipse.graphiti.platform.IDiagramEditor)
-	 */
 	public void init(Diagram diagram, IDiagramEditor diagramEditor) {
 		setDiagram(diagram);
 		setDiagramEditor(diagramEditor);
@@ -204,48 +133,23 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 		this.featureProvider = featureProvider;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#isAutoUpdateAtRuntime()
-	 */
 	public boolean isAutoUpdateAtRuntime() {
 		return true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#isAutoUpdateAtStartup()
-	 */
 	public boolean isAutoUpdateAtStartup() {
 		return false;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#isAutoUpdateAtReset()
-	 */
 	@Override
 	public boolean isAutoUpdateAtReset() {
 		return true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#getDiagramTitleImage()
-	 */
 	public String getDiagramTitleImage() {
 		return IPlatformImageConstants.IMG_DIAGRAM;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#dispose()
-	 */
 	public void dispose() {
 		if (getCurrentToolBehaviorProvider() != null) {
 			getCurrentToolBehaviorProvider().dispose();
@@ -255,45 +159,21 @@ public abstract class AbstractDiagramTypeProvider extends AbstractExtension impl
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#getNotificationService()
-	 */
 	public INotificationService getNotificationService() {
-		if (notificationService == null) {
-			notificationService = new DefaultNotificationService(this);
+		if (this.notificationService == null) {
+			this.notificationService = new DefaultNotificationService(this);
 		}
-		return notificationService;
+		return this.notificationService;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.dt.IDiagramTypeProvider#getRelatedBusinessObjects(
-	 * java.lang.Object[])
-	 */
 	public Object[] getRelatedBusinessObjects(Object[] bos) {
 		return new Object[0];
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.graphiti.dt.IDiagramTypeProvider#
-	 * getGraphicsAlgorithmRendererFactory()
-	 */
 	public IGraphicsAlgorithmRendererFactory getGraphicsAlgorithmRendererFactory() {
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.graphiti.dt.IDiagramTypeProvider#postInit()
-	 */
 	public void postInit() {
 	}
 
