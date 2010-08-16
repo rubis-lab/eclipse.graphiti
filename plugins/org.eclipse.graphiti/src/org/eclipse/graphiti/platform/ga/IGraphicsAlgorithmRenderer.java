aa47ffdde6907458de46bf101cfc8bbcf3c38f57
tkaiser <tkaiser>
2010-08-16 13:09:52 +0000

    javadoc

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/platform/ga/IGraphicsAlgorithmRenderer.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/platform/ga/IGraphicsAlgorithmRenderer.java
@@ -15,8 +15,16 @@
  *******************************************************************************/
 package org.eclipse.graphiti.platform.ga;
 
+import org.eclipse.graphiti.dt.IDiagramTypeProvider;
+
 /**
- * The Interface IGraphicsAlgorithmRenderer.
+ * A marker interface enabling the framework to distinguish between customer
+ * draw 2d figures and draw 2d figures created by the framework based on the
+ * Graphiti model. If an editor needs to provide custom draw 2d figures these
+ * figures need to implement this interface.
+ * 
+ * @see IGraphicsAlgorithmRendererFactory,
+ * @see IDiagramTypeProvider#getGraphicsAlgorithmRendererFactory
  */
 public interface IGraphicsAlgorithmRenderer {
 }
