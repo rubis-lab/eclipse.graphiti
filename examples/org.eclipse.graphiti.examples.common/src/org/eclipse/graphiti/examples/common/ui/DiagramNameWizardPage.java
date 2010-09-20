7d0da6e760f41e1d09584cbb69d4c08fa23a27d8
mgorning <mgorning>
2010-09-20 13:03:32 +0000

    DiagramWizard -> pages merged

--- examples/org.eclipse.graphiti.examples.common/src/org/eclipse/graphiti/examples/common/ui/DiagramNameWizardPage.java
+++ examples/org.eclipse.graphiti.examples.common/src/org/eclipse/graphiti/examples/common/ui/DiagramNameWizardPage.java
@@ -17,13 +17,39 @@ package org.eclipse.graphiti.examples.common.ui;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 
 /**
  * The Class DiagramNameWizardPage.
  */
-public class DiagramNameWizardPage extends AbstractInputFieldWizardPage {
+public class DiagramNameWizardPage extends AbstractWizardPage {
+
+	// constants
+	private static final int SIZING_TEXT_FIELD_WIDTH = 250;
+
+	// widgets
+	/**
+	 * The text field.
+	 */
+	Text textField;
+
+	private Listener nameModifyListener = new Listener() {
+		public void handleEvent(Event e) {
+			boolean valid = validatePage();
+			setPageComplete(valid);
+
+		}
+	};
 
 	/**
 	 * The Constructor.
@@ -49,53 +75,107 @@ public class DiagramNameWizardPage extends AbstractInputFieldWizardPage {
 		super(pageName);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.examples.testview.wizard.AbstractInputFieldWizardPage
-	 * #getInitialTextFieldValue()
-	 */
 	@Override
-	String getInitialTextFieldValue() {
+	public void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		composite.setFont(parent.getFont());
+
+		initializeDialogUnits(parent);
+
+		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		createWizardContents(composite);
+
+		setPageComplete(validatePage());
+
+		// Show description on opening
+		setErrorMessage(null);
+		setMessage(null);
+		setControl(composite);
+	}
+
+	public String getText() {
+		if (textField == null) {
+			return getInitialTextFieldValue();
+		}
+
+		return getTextFieldValue();
+	}
+
+	/**
+	 * Validate page.
+	 * 
+	 * @return true, if successful
+	 */
+	protected boolean validatePage() {
+		String text = getTextFieldValue();
+		if (text.equals("")) { //$NON-NLS-1$
+			setErrorMessage(null);
+			setMessage("Please enter diagram name");//$NON-NLS-1$
+			return false;
+		}
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IStatus status = doWorkspaceValidation(workspace, text);
+		if (!status.isOK()) {
+			setErrorMessage(status.getMessage());
+			return false;
+		}
+
+		setErrorMessage(null);
+		setMessage(null);
+		return true;
+	}
+
+	@Override
+	protected void createWizardContents(Composite parent) {
+		createProjectNameGroup(parent);
+	}
+
+	private final void createProjectNameGroup(Composite parent) {
+		// project specification group
+		Composite projectGroup = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		projectGroup.setLayout(layout);
+		projectGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		// new project label
+		Label projectLabel = new Label(projectGroup, SWT.NONE);
+		projectLabel.setText("Diagram Name");//$NON-NLS-1$
+		projectLabel.setFont(parent.getFont());
+
+		// new project name entry field
+		textField = new Text(projectGroup, SWT.BORDER);
+		GridData data = new GridData(GridData.FILL_HORIZONTAL);
+		data.widthHint = SIZING_TEXT_FIELD_WIDTH;
+		textField.setLayoutData(data);
+		textField.setFont(parent.getFont());
+
+		// Set the initial value first before listener
+		// to avoid handling an event during the creation.
+		if (getInitialTextFieldValue() != null) {
+			textField.setText(getInitialTextFieldValue());
+		}
+		textField.addListener(SWT.Modify, nameModifyListener);
+	}
+
+	private String getTextFieldValue() {
+		if (textField == null) {
+			return ""; //$NON-NLS-1$
+		}
+
+		return textField.getText().trim();
+	}
+
+	private String getInitialTextFieldValue() {
 		return "new diagram"; //$NON-NLS-1$
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.examples.testview.wizard.AbstractInputFieldWizardPage
-	 * #getLabelText()
-	 */
-	@Override
-	String getLabelText() {
-		return "Diagram Name";
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.examples.testview.wizard.AbstractInputFieldWizardPage
-	 * #getMessageText()
-	 */
-	@Override
-	String getMessageText() {
-		return "Please enter diagram name";
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.graphiti.samples.testview.wizard.AbstractInputFieldWizardPage
-	 * #doWorkspaceValidation(org.eclipse.core.resources.IWorkspace,
-	 * java.lang.String)
-	 */
-	@Override
-	IStatus doWorkspaceValidation(IWorkspace workspace, String text) {
+	private IStatus doWorkspaceValidation(IWorkspace workspace, String text) {
 		IStatus ret = workspace.validateName(text, IResource.FILE);
 		return ret;
 	}
+
 }
