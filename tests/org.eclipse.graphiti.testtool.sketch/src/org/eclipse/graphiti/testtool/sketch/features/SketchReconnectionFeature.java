06597901616c828f1966971ca662abc389681d94
cbrand <cbrand>
2011-03-04 10:11:53 +0000

    reconnect uses services

--- tests/org.eclipse.graphiti.testtool.sketch/src/org/eclipse/graphiti/testtool/sketch/features/SketchReconnectionFeature.java
+++ tests/org.eclipse.graphiti.testtool.sketch/src/org/eclipse/graphiti/testtool/sketch/features/SketchReconnectionFeature.java
@@ -1,11 +1,9 @@
 package org.eclipse.graphiti.testtool.sketch.features;
 
-import org.eclipse.graphiti.datatypes.ILocation;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.context.IReconnectionContext;
 import org.eclipse.graphiti.features.context.impl.ReconnectionContext;
 import org.eclipse.graphiti.features.impl.DefaultReconnectionFeature;
-import org.eclipse.graphiti.mm.algorithms.Ellipse;
 import org.eclipse.graphiti.mm.algorithms.Polyline;
 import org.eclipse.graphiti.mm.algorithms.styles.LineStyle;
 import org.eclipse.graphiti.mm.pictograms.Anchor;
@@ -15,11 +13,10 @@ import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.services.ICreateService;
+import org.eclipse.graphiti.testtool.sketch.SketchUtil;
 import org.eclipse.graphiti.util.IColorConstant;
 
 public class SketchReconnectionFeature extends DefaultReconnectionFeature {
-
-	private static final int POINT_SIZE = 10;
 
 	public SketchReconnectionFeature(IFeatureProvider fp) {
 		super(fp);
@@ -51,18 +48,9 @@ public class SketchReconnectionFeature extends DefaultReconnectionFeature {
 		if (targetPictogramElement instanceof FreeFormConnection) {
 			Connection targetConnection = (Connection) targetPictogramElement;
 			ICreateService createService = Graphiti.getCreateService();
-			Shape newShape = createService.createShape(getDiagram(), true);
-			Ellipse ellipse = createService.createEllipse(newShape);
-			int x = 0, y = 0;
-			if (context instanceof ReconnectionContext) {
-				ILocation targetLocation = ((ReconnectionContext) context).getTargetLocation();
-				x = targetLocation.getX();
-				y = targetLocation.getY();
-			}
-			Graphiti.getLayoutService().setLocationAndSize(ellipse, x - POINT_SIZE / 2, y - POINT_SIZE / 2, POINT_SIZE, POINT_SIZE);
-			ellipse.setFilled(true);
-			ellipse.setForeground(manageColor(IColorConstant.LIGHT_BLUE));
-			Anchor newAnchor = createService.createChopboxAnchor(newShape);
+
+			Shape connectionPointShape = SketchUtil.createConnectionPoint(context, getDiagram());
+			Anchor newAnchor = createService.createChopboxAnchor(connectionPointShape);
 
 			Anchor targetStart = targetConnection.getStart();
 			Anchor targetEnd = targetConnection.getEnd();
@@ -82,7 +70,7 @@ public class SketchReconnectionFeature extends DefaultReconnectionFeature {
 			if (context instanceof ReconnectionContext) {
 				ReconnectionContext rc = (ReconnectionContext) context;
 				rc.setNewAnchor(newAnchor);
-				rc.setTargetPictogramElement(newShape);
+				rc.setTargetPictogramElement(connectionPointShape);
 			}
 		}
 		super.preReconnect(context);
