abc732a25aab31a227b9be014b6ce7c74103aa0c
tkaiser <tkaiser>
2011-03-07 13:31:02 +0000

    fix

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/internal/services/impl/MigrationServiceImpl.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/internal/services/impl/MigrationServiceImpl.java
@@ -15,7 +15,9 @@
  *******************************************************************************/
 package org.eclipse.graphiti.internal.services.impl;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
@@ -29,12 +31,11 @@ import org.eclipse.graphiti.services.IMigrationService;
 
 public class MigrationServiceImpl implements IMigrationService {
 
-
 	@Override
 	public void migrate070To080(Diagram d) {
 
 		// Traverse model and collect fonts
-		HashSet<Font> fonts = new HashSet<Font>();
+		Map<Font, ArrayList<EObject>> fonts = new HashMap<Font, ArrayList<EObject>>();
 		Resource eResource = d.eResource();
 		TreeIterator<EObject> allContents = eResource.getAllContents();
 		while (allContents.hasNext()) {
@@ -42,20 +43,37 @@ public class MigrationServiceImpl implements IMigrationService {
 			if (next instanceof AbstractText) {
 				AbstractText t = (AbstractText) next;
 				Font font = t.getFont();
-				if (font != null)
-					fonts.add(font);
+				addFontUser(fonts, next, font);
 			} else if (next instanceof Style) {
 				Style s = (Style) next;
 				Font font = s.getFont();
-				if (font != null)
-					fonts.add(font);
+				addFontUser(fonts, next, font);
 			}
 		}
 
-		// Manage collected fonts, including possibly a write.
+		// Manage collected fonts and set the new font on the respective
+		// elements. Includes possibly a write.
 		// The caller has to use a write transaction.
-		for (Font font : fonts) {
-			Graphiti.getGaService().manageFont(d, font.getName(), font.getSize(), font.isItalic(), font.isBold());
+		for (Font font : fonts.keySet()) {
+			Font newFont = Graphiti.getGaService().manageFont(d, font.getName(), font.getSize(), font.isItalic(), font.isBold());
+			ArrayList<EObject> arrayList = fonts.get(font);
+			for (EObject eObject : arrayList) {
+				if (eObject instanceof AbstractText) {
+					((AbstractText) eObject).setFont(newFont);
+				} else {
+					((Style) eObject).setFont(newFont);
+				}
+			}
+		}
+	}
+
+
+	private void addFontUser(Map<Font, ArrayList<EObject>> fonts, EObject next, Font font) {
+		if (font != null) {
+			if (fonts.get(font) == null) {
+				fonts.put(font, new ArrayList<EObject>());
+			}
+			fonts.get(font).add(next);
 		}
 	}
 
