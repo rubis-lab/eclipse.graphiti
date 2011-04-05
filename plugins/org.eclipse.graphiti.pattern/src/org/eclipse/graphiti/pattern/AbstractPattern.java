8e47d0cb54a9890bf72c17226be43807c07e6027
cbrand <cbrand>
2011-04-05 12:36:44 +0000

    .

--- plugins/org.eclipse.graphiti.pattern/src/org/eclipse/graphiti/pattern/AbstractPattern.java
+++ plugins/org.eclipse.graphiti.pattern/src/org/eclipse/graphiti/pattern/AbstractPattern.java
@@ -718,14 +718,14 @@ public abstract class AbstractPattern extends AbstractBasePattern implements IPa
 	@Override
 	public IProposal[] getPossibleValuesAsProposal(IDirectEditingContext context) {
 		String[] possibleValues = getPossibleValues(context);
-		Proposal[] ret = Proposal.textToProposals(possibleValues);
+		Proposal[] ret = Proposal.textsToProposals(possibleValues);
 		return ret;
 	}
 
 	@Override
 	public IProposal[] getValueProposalsAsProposal(String value, int caretPosition, IDirectEditingContext context) {
 		String[] possibleValues = getValueProposals(value, caretPosition, context);
-		Proposal[] ret = Proposal.textToProposals(possibleValues);
+		Proposal[] ret = Proposal.textsToProposals(possibleValues);
 		return ret;
 	}
 
