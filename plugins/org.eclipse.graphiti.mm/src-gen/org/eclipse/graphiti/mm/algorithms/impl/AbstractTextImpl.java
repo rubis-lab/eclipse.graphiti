b68b7cae48907994516182e9249d24c28ff04d72
cbrand <cbrand>
2011-06-03 08:57:19 +0000

    .

--- plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/impl/AbstractTextImpl.java
+++ plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/impl/AbstractTextImpl.java
@@ -15,7 +15,6 @@
 package org.eclipse.graphiti.mm.algorithms.impl;
 
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.common.notify.NotificationChain;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
