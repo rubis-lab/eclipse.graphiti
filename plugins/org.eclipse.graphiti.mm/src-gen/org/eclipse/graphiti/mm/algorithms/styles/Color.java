0c8cb70b848e230cfe4854147fd16d1677d6c3d5
mwenz <michael.wenz@sap.com>
2011-09-06 15:25:57 +0200

    Bug 355347 - Updated file headers

--- plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/styles/Color.java
+++ plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/styles/Color.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2005, 2010 SAP AG.
+ * Copyright (c) 2005, 2011 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,65 +17,73 @@ package org.eclipse.graphiti.mm.algorithms.styles;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Color</b></em>'.
- * <!-- end-user-doc -->
- *
+ * <!-- begin-user-doc --> A representation of the model object '
+ * <em><b>Color</b></em>'. <!-- end-user-doc -->
+ * 
  * <p>
  * The following features are supported:
  * <ul>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getRed <em>Red</em>}</li>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getGreen <em>Green</em>}</li>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getBlue <em>Blue</em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getRed <em>Red
+ * </em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getGreen <em>Green
+ * </em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Color#getBlue <em>Blue
+ * </em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getColor()
  * @model
  * @generated
  */
 public interface Color extends EObject {
 	/**
-	 * Returns the value of the '<em><b>Red</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Red</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Red</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Red</em>' attribute isn't clear, there really
+	 * should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Red</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getColor_Red()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	int getRed();
 
 	/**
-	 * Returns the value of the '<em><b>Green</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Green</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Green</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Green</em>' attribute isn't clear, there
+	 * really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Green</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getColor_Green()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	int getGreen();
 
 	/**
-	 * Returns the value of the '<em><b>Blue</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Blue</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Blue</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Blue</em>' attribute isn't clear, there really
+	 * should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Blue</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getColor_Blue()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	int getBlue();
--- plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/styles/Font.java
+++ plugins/org.eclipse.graphiti.mm/src-gen/org/eclipse/graphiti/mm/algorithms/styles/Font.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2005, 2010 SAP AG.
+ * Copyright (c) 2005, 2011 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,81 +17,92 @@ package org.eclipse.graphiti.mm.algorithms.styles;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Font</b></em>'.
- * <!-- end-user-doc -->
- *
+ * <!-- begin-user-doc --> A representation of the model object '
+ * <em><b>Font</b></em>'. <!-- end-user-doc -->
+ * 
  * <p>
  * The following features are supported:
  * <ul>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#getName <em>Name</em>}</li>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#getSize <em>Size</em>}</li>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#isItalic <em>Italic</em>}</li>
- *   <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#isBold <em>Bold</em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#getName <em>Name
+ * </em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#getSize <em>Size
+ * </em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#isItalic <em>Italic
+ * </em>}</li>
+ * <li>{@link org.eclipse.graphiti.mm.algorithms.styles.Font#isBold <em>Bold
+ * </em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getFont()
  * @model
  * @generated
  */
 public interface Font extends EObject {
 	/**
-	 * Returns the value of the '<em><b>Name</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Name</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear, there really
+	 * should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Name</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getFont_Name()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	String getName();
 
 	/**
-	 * Returns the value of the '<em><b>Size</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Size</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Size</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Size</em>' attribute isn't clear, there really
+	 * should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Size</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getFont_Size()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	int getSize();
 
 	/**
-	 * Returns the value of the '<em><b>Italic</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Italic</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Italic</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Italic</em>' attribute isn't clear, there
+	 * really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Italic</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getFont_Italic()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	boolean isItalic();
 
 	/**
-	 * Returns the value of the '<em><b>Bold</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Bold</b></em>' attribute. <!--
+	 * begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Bold</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Bold</em>' attribute isn't clear, there really
+	 * should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Bold</em>' attribute.
 	 * @see org.eclipse.graphiti.mm.algorithms.styles.StylesPackage#getFont_Bold()
-	 * @model unique="false" required="true" ordered="false" suppressedSetVisibility="true"
+	 * @model unique="false" required="true" ordered="false"
+	 *        suppressedSetVisibility="true"
 	 * @generated
 	 */
 	boolean isBold();
