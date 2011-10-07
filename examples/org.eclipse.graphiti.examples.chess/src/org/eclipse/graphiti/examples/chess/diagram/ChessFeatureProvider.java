196897ace56949c3675843f938162f0351ebea0d
mwenz <michael.wenz@sap.com>
2011-10-07 14:31:44 +0200

    Chess: disable additional features

--- examples/org.eclipse.graphiti.examples.chess/src/org/eclipse/graphiti/examples/chess/diagram/ChessFeatureProvider.java
+++ examples/org.eclipse.graphiti.examples.chess/src/org/eclipse/graphiti/examples/chess/diagram/ChessFeatureProvider.java
@@ -18,10 +18,19 @@ import org.eclipse.graphiti.dt.IDiagramTypeProvider;
 import org.eclipse.graphiti.examples.chess.features.AddChessBoardFeature;
 import org.eclipse.graphiti.examples.chess.features.CreateChessBoardFeature;
 import org.eclipse.graphiti.examples.mm.chess.Board;
+import org.eclipse.graphiti.examples.mm.chess.Square;
 import org.eclipse.graphiti.features.IAddFeature;
 import org.eclipse.graphiti.features.ICreateFeature;
+import org.eclipse.graphiti.features.IDeleteFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.IMoveShapeFeature;
+import org.eclipse.graphiti.features.IRemoveFeature;
+import org.eclipse.graphiti.features.IResizeShapeFeature;
 import org.eclipse.graphiti.features.context.IAddContext;
+import org.eclipse.graphiti.features.context.IDeleteContext;
+import org.eclipse.graphiti.features.context.IMoveShapeContext;
+import org.eclipse.graphiti.features.context.IRemoveContext;
+import org.eclipse.graphiti.features.context.IResizeShapeContext;
 import org.eclipse.graphiti.ui.features.DefaultFeatureProvider;
 
 public class ChessFeatureProvider extends DefaultFeatureProvider implements IFeatureProvider {
@@ -41,5 +50,61 @@ public class ChessFeatureProvider extends DefaultFeatureProvider implements IFea
 			return new AddChessBoardFeature(this);
 		}
 		return super.getAddFeature(context);
+	}
+
+	@Override
+	public IRemoveFeature getRemoveFeature(IRemoveContext context) {
+		Object bo = getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if (bo instanceof Board) {
+			// No removal allowed for the board
+			return null;
+		}
+		if (bo instanceof Square) {
+			// No removal allowed for squares
+			return null;
+		}
+
+		return super.getRemoveFeature(context);
+	}
+
+	@Override
+	public IDeleteFeature getDeleteFeature(IDeleteContext context) {
+		Object bo = getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if (bo instanceof Board) {
+			// No deletion allowed for the board
+			return null;
+		}
+		if (bo instanceof Square) {
+			// No deletion allowed for squares
+			return null;
+		}
+
+		return super.getDeleteFeature(context);
+	}
+
+	@Override
+	public IResizeShapeFeature getResizeShapeFeature(IResizeShapeContext context) {
+		Object bo = getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if (bo instanceof Board) {
+			// No resize allowed for the board
+			return null;
+		}
+		if (bo instanceof Square) {
+			// No resize allowed for squares
+			return null;
+		}
+
+		return super.getResizeShapeFeature(context);
+	}
+
+	@Override
+	public IMoveShapeFeature getMoveShapeFeature(IMoveShapeContext context) {
+		Object bo = getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if (bo instanceof Square) {
+			// No move allowed for squares
+			return null;
+		}
+
+		return super.getMoveShapeFeature(context);
 	}
 }
