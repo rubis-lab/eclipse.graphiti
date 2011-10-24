bd2a9a6a277dcdd11b9d6e5de6c4ad66136777a8
tkaiser <tim.kaiser@sap.com>
2011-10-24 13:55:38 +0200

    RESOLVED - bug 360800: Text background color is ignored https://bugs.eclipse.org/bugs/show_bug.cgi?id=360800
    use styles

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/parts/PictogramElementDelegate.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/parts/PictogramElementDelegate.java
@@ -557,7 +557,7 @@ public class PictogramElementDelegate implements IPictogramElementDelegate {
 			label.setText(text.getValue());
 			refreshFlowTextAlignment(label, text);
 			refreshFont(text, label);
-			label.setOpaque(text.getFilled());
+			label.setOpaque(Graphiti.getGaService().isFilled(text, true));
 			label.setRequestFocusEnabled(false);
 			label.invalidateTree();
 		} else if (graphicsAlgorithm instanceof Text && figure instanceof GFText) {
@@ -566,7 +566,7 @@ public class PictogramElementDelegate implements IPictogramElementDelegate {
 			label.setText(text.getValue());
 			refreshTextOrientation(label, text);
 			refreshFont(text, label);
-			label.setOpaque(text.getFilled());
+			label.setOpaque(Graphiti.getGaService().isFilled(text, true));
 			label.setRequestFocusEnabled(false);
 		} else if (graphicsAlgorithm instanceof Image && figure instanceof ImageFigure) {
 			ImageFigure imageFigure = (ImageFigure) figure;
