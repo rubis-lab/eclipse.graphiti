8771b53919d45a6cf9de2d71dd8901a5ceec29ea
mwenz <michael.wenz@sap.com>
2012-01-20 16:08:58 +0100

    Bug 342869 - Applied patch

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/figures/GFImageFigure.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/figures/GFImageFigure.java
@@ -19,6 +19,9 @@ import org.eclipse.draw2d.Graphics;
 import org.eclipse.graphiti.internal.services.GraphitiInternal;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * @noinstantiate This class is not intended to be instantiated by clients.
@@ -27,8 +30,69 @@ import org.eclipse.graphiti.services.Graphiti;
 public class GFImageFigure extends ImageFigureAntialias {
 	private GraphicsAlgorithm graphicsAlgorithm;
 
+	private Image fillImage = null;
+
 	public GFImageFigure(GraphicsAlgorithm graphicsAlgorithm) {
 		this.graphicsAlgorithm = graphicsAlgorithm;
+	}
+
+	@Override
+	public void setImage(Image image) {
+		if (image != null
+				&& graphicsAlgorithm instanceof org.eclipse.graphiti.mm.algorithms.Image) {
+			org.eclipse.graphiti.mm.algorithms.Image imageGA = (org.eclipse.graphiti.mm.algorithms.Image) graphicsAlgorithm;
+			if (imageGA.getStretchH() || imageGA.getStretchV()) {
+				// scaling required
+				double scalefactorX = 1.0;
+				double scalefactorY = 1.0;
+				// set initial scalefactor
+				// get image data from default image
+				ImageData originalImageData = image.getImageData();
+				int imageWidth = originalImageData.width;
+				int imageHeight = originalImageData.height;
+				if (imageGA.getStretchH()) {
+					scalefactorX = (double) graphicsAlgorithm.getWidth()
+							/ (double) imageWidth;
+				}
+				if (imageGA.getStretchV()) {
+					scalefactorY = (double) graphicsAlgorithm.getHeight()
+							/ (double) imageHeight;
+				}
+				if (imageGA.getProportional()) {
+					// scale down the image to match into frame
+					// both dimensions are scaled to the smaller scalefactor in
+					// order to make the image matching into the container
+					double actualScalefactor = 0;
+					if (scalefactorX < scalefactorY) {
+						actualScalefactor = scalefactorX;
+					} else {
+						actualScalefactor = scalefactorY;
+					}
+					scalefactorX = actualScalefactor;
+					scalefactorY = actualScalefactor;
+				}
+				// create scaled image
+				double d = imageWidth * scalefactorX;
+				double e = imageHeight * scalefactorY;
+				Image oldFillImage = fillImage;
+				fillImage = new Image(Display.getCurrent(),
+						originalImageData.scaledTo((int) d, (int) e));
+				super.setImage(fillImage);
+				// keep track of the old fill image on resize and dispose it to
+				// avoid memory leaks
+				// We don't change the image in
+				// GraphitiUIPlugin.getDefault().getImageRegistry() because
+				// scaling down makes image quality bad
+				if (oldFillImage != null && !oldFillImage.isDisposed()) {
+					oldFillImage.dispose();
+				}
+			} else {
+				super.setImage(image);
+			}
+		} else {
+			super.setImage(image);
+		}
+
 	}
 
 	@Override
