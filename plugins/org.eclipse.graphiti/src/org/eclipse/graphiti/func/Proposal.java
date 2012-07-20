afb8a545afeb6f74162fcb0b06c7f80bc6b6e068
mwenz <michael.wenz@sap.com>
2012-07-20 12:12:42 +0200

    Test
    Change-Id: I688230499ca3e1ab4d434d4772c918d2fd6ad42b

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/func/Proposal.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/func/Proposal.java
@@ -35,17 +35,21 @@ public class Proposal implements IProposal {
 		return object;
 	}
 
+
 	public String getText() {
 		return text; // + " - " + getObject(); // for tests
 	}
+
 
 	public void setObject(Object object) {
 		this.object = object;
 	}
 
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 
 	public static Proposal[] textsToProposals(String[] texts) {
 		Proposal[] ret = new Proposal[texts.length];
@@ -55,6 +59,7 @@ public class Proposal implements IProposal {
 		return ret;
 	}
 
+
 	public static String[] proposalsToTexts(IProposal[] proposals) {
 		String[] ret = new String[proposals.length];
 		for (int i = 0; i < ret.length; i++) {
@@ -62,5 +67,4 @@ public class Proposal implements IProposal {
 		}
 		return ret;
 	}
-
 }
