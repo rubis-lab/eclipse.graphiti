5f4f897b0db639151d4013d706d0d8894bdc61d5
mwenz <michael.wenz@sap.com>
2012-08-09 16:14:28 +0200

    Version update to 0.9.1
    Change-Id: I7ea63c0414015dd08194e174f8ae33f7d7ee3370

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/editor/DefaultPersistencyBehavior.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/editor/DefaultPersistencyBehavior.java
@@ -134,7 +134,7 @@ public class DefaultPersistencyBehavior {
 	 * {@link DiagramEditor} so that the complete state of all modified objects
 	 * will be persisted in the file system.<br>
 	 * The default implementation also sets the current version information
-	 * (currently 0.9.0) to the diagram before saving it and wraps the save
+	 * (currently 0.9.1) to the diagram before saving it and wraps the save
 	 * operation inside a {@link IRunnableWithProgress} that cares about sending
 	 * only one {@link Resource} change event holding all modified files.
 	 * Besides also all adapters are temporarily switched off (see
@@ -267,7 +267,7 @@ public class DefaultPersistencyBehavior {
 	/**
 	 * Called in {@link #saveDiagram(IProgressMonitor)} to update the Graphiti
 	 * diagram version before saving a diagram. Currently the diagram version is
-	 * set to 0.9.0
+	 * set to 0.9.1
 	 * 
 	 * @param diagram
 	 *            the {@link Diagram} to update the version attribute for
