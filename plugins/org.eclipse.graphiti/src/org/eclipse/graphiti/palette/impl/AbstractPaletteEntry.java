0eb54b4b073b2d51b6fa5455ff5afbfd51ad738c
mwenz <michael.wenz@sap.com>
2012-08-10 10:45:21 +0200

    Test for Gerrit build job
    Change-Id: I5fd9fa1ad957d9d3c7817fb771a1610c1cd03177

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/palette/impl/AbstractPaletteEntry.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/palette/impl/AbstractPaletteEntry.java
@@ -1,29 +1,21 @@
 /*******************************************************************************
  * <copyright>
- *
  * Copyright (c) 2005, 2010 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
  *    SAP AG - initial API, implementation and documentation
- *
  * </copyright>
- *
  *******************************************************************************/
 package org.eclipse.graphiti.palette.impl;
-
 import org.eclipse.graphiti.palette.IPaletteEntry;
-
 /**
  * The Class AbstractPaletteEntry.
  */
 public abstract class AbstractPaletteEntry implements IPaletteEntry {
-
 	private String label, iconId;
-
 	/**
 	 * Creates a new {@link AbstractPaletteEntry}.
 	 * 
@@ -36,13 +28,10 @@ public abstract class AbstractPaletteEntry implements IPaletteEntry {
 		this.label = label;
 		this.iconId = iconId;
 	}
-
 	public String getLabel() {
 		return this.label;
 	}
-
 	public String getIconId() {
 		return this.iconId;
 	}
-
 }
