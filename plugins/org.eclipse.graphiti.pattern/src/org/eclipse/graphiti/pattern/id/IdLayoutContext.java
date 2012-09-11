08aa0f07e3866c5fe7d05eabefed486c3451c70a
mwenz <michael.wenz@sap.com>
2012-09-11 16:22:36 +0200

    Bug 385193 - Provide ID pattern to lower development effort
    * represent files as rectangles
    
    Change-Id: Ibaf18fbea68cdea47069213dd45356563cd2c7aa

---
+++ plugins/org.eclipse.graphiti.pattern/src/org/eclipse/graphiti/pattern/id/IdLayoutContext.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * <copyright>
+ *
+ * Copyright (c) 2012, 2012 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API, implementation and documentation
+ *
+ * </copyright>
+ *
+ *******************************************************************************/
+package org.eclipse.graphiti.pattern.id;
+
+import org.eclipse.graphiti.features.context.impl.LayoutContext;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+
+/**
+ * @since 0.10
+ */
+public class IdLayoutContext extends LayoutContext {
+
+	private GraphicsAlgorithm graphicsAlgorithm;
+	private PictogramElement rootPictogramElement;
+
+	public IdLayoutContext(PictogramElement pictogramElement, GraphicsAlgorithm graphicsAlgorithm,
+			PictogramElement rootPictogramElement) {
+		super(pictogramElement);
+		this.graphicsAlgorithm = graphicsAlgorithm;
+		this.rootPictogramElement = rootPictogramElement;
+	}
+
+	public GraphicsAlgorithm getGraphicsAlgorithm() {
+		return graphicsAlgorithm;
+	}
+
+	public void setGraphicsAlgorithm(GraphicsAlgorithm graphicsAlgorithm) {
+		this.graphicsAlgorithm = graphicsAlgorithm;
+	}
+
+	public PictogramElement getRootPictogramElement() {
+		return rootPictogramElement;
+	}
+
+	public void setRootPictogramElement(PictogramElement rootPictogramElement) {
+		this.rootPictogramElement = rootPictogramElement;
+	}
+}
