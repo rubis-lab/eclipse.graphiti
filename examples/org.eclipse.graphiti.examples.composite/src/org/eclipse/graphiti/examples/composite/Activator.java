9b7c3e6be90a6e4d733f3cf01b0ebae6ee00d7a5
Patrick Paulin <patrick@modumind.com>
2013-02-05 11:37:41 -0600

    Bug 352120 - Updated copyrights and some clean up

--- examples/org.eclipse.graphiti.examples.composite/src/org/eclipse/graphiti/examples/composite/Activator.java
+++ examples/org.eclipse.graphiti.examples.composite/src/org/eclipse/graphiti/examples/composite/Activator.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * <copyright>
  *
- * Copyright (c) 2005, 2011 SAP AG.
+ * Copyright (c) 2013 SRC
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
