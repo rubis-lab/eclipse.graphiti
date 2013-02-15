2d40a1674c8ade347a3394ed272232af275abfb0
Patrick Paulin <patrick@modumind.com>
2013-02-15 14:44:42 +0100

    Bug 352120 - Updated copyrights and some clean up
    Change-Id: I7d2a12d64361f8f98090bc39e01704d4ed74c27e

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/editor/DiagramSupport.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/editor/DiagramSupport.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * <copyright>
  *
- * Copyright (c) 2011, 2013 Modular Mind, Ltd.
+ * Copyright (c) 2013 SRC
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
