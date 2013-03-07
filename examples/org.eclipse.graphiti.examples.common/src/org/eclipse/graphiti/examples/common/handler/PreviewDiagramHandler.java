bf9400e61556ec74e03d61938010763388af6917
Felix Velasco <felix.velasco@gmail.com>
2013-03-07 09:09:35 +0100

    Add an example showing two diagrams, side by side, in a pop-up dialog
    Change-Id: Ie304e7526d202182d588cf30fdd4afa7ade94912

---
+++ examples/org.eclipse.graphiti.examples.common/src/org/eclipse/graphiti/examples/common/handler/PreviewDiagramHandler.java
@@ -0,0 +1,30 @@
+package org.eclipse.graphiti.examples.common.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class PreviewDiagramHandler extends AbstractHandler implements IHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IStructuredSelection currentSelection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
+		Object[] elements = currentSelection.toArray();
+
+		IResource res1 = (IResource) Platform.getAdapterManager().getAdapter(elements[0], IResource.class);
+		IResource res2 = (IResource) Platform.getAdapterManager().getAdapter(elements[1], IResource.class);
+
+		if (res1 != null && res2 != null) {
+
+			DoubleDiagramDialog dialog = new DoubleDiagramDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getShell(), res1, res2);
+			dialog.open();
+		}
+		return null;
+	}
+}
