b9a671dc7d4b9f75481d816e80fe806668cfd4ec
mwenz <michael.wenz@sap.com>
2013-05-28 09:31:12 +0200

    Consolidate composite examples
    *Moved view example into own package in composite example
    *Moved popup example to composite example
    *Added some docu
    *Added/reworked copyrights and other necessary files
    *Added to examples feature
    *Changed to standard formatting
    
    Change-Id: I56273ec6980cef11f37c7bb3f57c6b24ec68f7eb

--- examples/org.eclipse.graphiti.examples.composite/src/org/eclipse/graphiti/examples/composite/Activator.java
+++ examples/org.eclipse.graphiti.examples.composite/src/org/eclipse/graphiti/examples/composite/Activator.java
@@ -28,16 +28,20 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
+		super();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -46,7 +50,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -55,11 +62,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
-
 }
