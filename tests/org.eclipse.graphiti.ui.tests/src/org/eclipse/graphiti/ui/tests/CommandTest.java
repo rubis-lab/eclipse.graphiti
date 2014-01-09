d5ad50a76728122c8106a2dc964c4c030d60d134
Michael Wenz <michael.wenz@sap.com>
2014-01-09 14:43:29 +0100

    Clean-up
    *Added header comment for CommandTest
    *Masked warnings
    
    Change-Id: Ib116292f0e12638d7fe939b6177c0caed61f88e9

--- tests/org.eclipse.graphiti.ui.tests/src/org/eclipse/graphiti/ui/tests/CommandTest.java
+++ tests/org.eclipse.graphiti.ui.tests/src/org/eclipse/graphiti/ui/tests/CommandTest.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * <copyright>
+ *
+ * Copyright (c) 2012, 2013 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SAP AG - initial API, implementation and documentation
+ *    mlypik - Bug 401792 - Disable starting reconnection
+ *
+ * </copyright>
+ *
+ *******************************************************************************/
 package org.eclipse.graphiti.ui.tests;
 
 import static org.easymock.EasyMock.createNiceMock;
