ba71ae03a48bf0650bae1d67decee85df5ef5a24
Michael Wenz <michael.wenz@sap.com>
2014-09-26 15:17:06 +0200

    Bug 443304 - Improve undo/redo handling in Graphiti features
    * Introduce ICustomUndoRedoFeature hosting
    ** both pre/post hooks for both undo/redo
    * Deprecate ICustomUndoableFeature
    * Call both interfaces in feature undo/redo
    * Introduce ICustomAbortableUndoRedoFeature hosting isAbort() hook
    ** Check isAbort after calling pre hooks
    
    Open
    * JavaDoc
    * Example
    * Docu
    
    Change-Id: I0e22d4f9182dab9f1adccf29daaac675c7d17680

---
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/features/ICustomAbortableUndoRedoFeature.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * <copyright>
+ *
+ * Copyright (c) 2014, 2014 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    mwenz - Bug 443304 - Improve undo/redo handling in Graphiti features
+ *
+ * </copyright>
+ *
+ *******************************************************************************/
+package org.eclipse.graphiti.features;
+
+
+/**
+ * @since 0.12
+ */
+public interface ICustomAbortableUndoRedoFeature extends ICustomUndoRedoFeature {
+
+	boolean isAbort();
+}
