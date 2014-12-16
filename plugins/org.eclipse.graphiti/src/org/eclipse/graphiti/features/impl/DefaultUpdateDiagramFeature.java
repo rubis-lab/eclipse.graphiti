345d60a463456750bb65b960f0f9addafba1a0d5
Michael Wenz <michael.wenz@sap.com>
2014-12-16 09:10:10 +0100

    Bug 453553 - Adapt DefaultUpdateDiagramFeature to abort possibility
    Change-Id: Iaed0f976e72651443dbc1f9b25b44162b2a1f926

--- plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/features/impl/DefaultUpdateDiagramFeature.java
+++ plugins/org.eclipse.graphiti/src/org/eclipse/graphiti/features/impl/DefaultUpdateDiagramFeature.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * <copyright>
  *
- * Copyright (c) 2005, 2011 SAP AG.
+ * Copyright (c) 2005, 2014 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,7 @@
  *    SAP AG - initial API, implementation and documentation
  *    mwenz - Bug 356218 - Added hasDoneChanges updates to update diagram feature
  *                         and called features via editor command stack to check it
+ *    Laurent Le Moux (mwenz) - Bug 453553 - Provide an abort possibility for delete and remove features in case 'pre' methods fail
  *
  * </copyright>
  *
@@ -21,9 +22,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.graphiti.features.ICustomAbortableUndoRedoFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.IReason;
 import org.eclipse.graphiti.features.IUpdateFeature;
+import org.eclipse.graphiti.features.context.IContext;
 import org.eclipse.graphiti.features.context.IUpdateContext;
 import org.eclipse.graphiti.features.context.impl.UpdateContext;
 import org.eclipse.graphiti.mm.pictograms.Connection;
@@ -37,7 +40,7 @@ import org.eclipse.graphiti.mm.pictograms.Shape;
  * since in many cases the connections get already updated by the shapes they
  * belong to.
  */
-public class DefaultUpdateDiagramFeature extends AbstractUpdateFeature {
+public class DefaultUpdateDiagramFeature extends AbstractUpdateFeature implements ICustomAbortableUndoRedoFeature {
 
 	private boolean hasDoneChanges = false;
 
@@ -57,29 +60,21 @@ public class DefaultUpdateDiagramFeature extends AbstractUpdateFeature {
 
 	public boolean update(IUpdateContext context) {
 		PictogramElement pe = context.getPictogramElement();
-		if (pe instanceof Diagram) {
-			Diagram d = (Diagram) pe;
-			EList<Shape> children = d.getChildren();
-			// Collect PEs for update. Execute update later, because otherwise we could get a 
-			// ConcurrentModificationException if PEs get deleted etc.
-			Map<IUpdateFeature, IUpdateContext> connToUpdate = new HashMap<IUpdateFeature, IUpdateContext>();
-			for (Shape shape : children) {
-				UpdateContext updateContext = new UpdateContext(shape);
-				IUpdateFeature updateFeature = getFeatureProvider().getUpdateFeature(updateContext);
-				if (updateFeature != null && updateFeature.canUpdate(updateContext)
-						&& updateFeature.updateNeeded(updateContext).toBoolean()) {
-					connToUpdate.put(updateFeature, updateContext);
-				}
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, true);
+
+		// Reset hasDoneChanges flag because no changes have happened so far
+		hasDoneChanges = false;
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature
+					&& ((ICustomAbortableUndoRedoFeature) feature).isAbort()) {
+				// Perform all updates but stop as soon as one is aborted
+				break;
 			}
 
-			// Reset hasDoneChanges flag because no changes have happened so far
-			hasDoneChanges = false;
-			for (IUpdateFeature feature : connToUpdate.keySet()) {
-				feature.update(connToUpdate.get(feature));
-				if (feature.hasDoneChanges()) {
-					// At least one sub feature did some changes
-					hasDoneChanges = true;
-				}
+			feature.update(connToUpdate.get(feature));
+			if (feature.hasDoneChanges()) {
+				// At least one sub feature did changes
+				hasDoneChanges = true;
 			}
 		}
 		return true;
@@ -117,5 +112,179 @@ public class DefaultUpdateDiagramFeature extends AbstractUpdateFeature {
 	@Override
 	public boolean hasDoneChanges() {
 		return hasDoneChanges;
+	}
+
+	@Override
+	public boolean canUndo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (!abortableFeature.canUndo(context)) {
+					// One sub feature cannot be undone
+					// --> disable undo for composite
+					return false;
+				}
+			}
+		}
+
+		return super.canUndo(context);
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public void preUndo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+
+		// Reset hasDoneChanges flag because no changes have happened so far
+		hasDoneChanges = false;
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (abortableFeature.isAbort()) {
+					// Perform all updates but stop as soon as one is
+					// aborted
+					break;
+				}
+				abortableFeature.preUndo(connToUpdate.get(feature));
+				if (feature.hasDoneChanges()) {
+					// At least one sub feature did some changes
+					hasDoneChanges = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public void postUndo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (abortableFeature.isAbort()) {
+					// Perform all updates but stop as soon as one is
+					// aborted
+					break;
+				}
+				abortableFeature.postUndo(connToUpdate.get(feature));
+				if (feature.hasDoneChanges()) {
+					// At least one sub feature did some changes
+					hasDoneChanges = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public boolean canRedo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (!abortableFeature.canRedo(context)) {
+					// One sub feature cannot be redone
+					// --> disable redo for composite
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public void preRedo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+
+		// Reset hasDoneChanges flag because no changes have happened so far
+		hasDoneChanges = false;
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (abortableFeature.isAbort()) {
+					// Perform all updates but stop as soon as one is
+					// aborted
+					break;
+				}
+				abortableFeature.preRedo(connToUpdate.get(feature));
+				if (feature.hasDoneChanges()) {
+					// At least one sub feature did some changes
+					hasDoneChanges = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public void postRedo(IContext context) {
+		PictogramElement pe = ((IUpdateContext) context).getPictogramElement();
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = findFeaturesToUpdateChildren(pe, false);
+
+		for (IUpdateFeature feature : connToUpdate.keySet()) {
+			if (feature instanceof ICustomAbortableUndoRedoFeature) {
+				ICustomAbortableUndoRedoFeature abortableFeature = (ICustomAbortableUndoRedoFeature) feature;
+				if (abortableFeature.isAbort()) {
+					// Perform all updates but stop as soon as one is
+					// aborted
+					break;
+				}
+				abortableFeature.postRedo(connToUpdate.get(feature));
+				if (feature.hasDoneChanges()) {
+					// At least one sub feature did some changes
+					hasDoneChanges = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	@Override
+	public boolean isAbort() {
+		return false;
+	}
+
+	/**
+	 * @since 0.12
+	 */
+	protected Map<IUpdateFeature, IUpdateContext> findFeaturesToUpdateChildren(PictogramElement pe,
+			boolean checkUpdateNeeded) {
+		Map<IUpdateFeature, IUpdateContext> connToUpdate = new HashMap<IUpdateFeature, IUpdateContext>();
+		if (pe instanceof Diagram) {
+			Diagram d = (Diagram) pe;
+			EList<Shape> children = d.getChildren();
+			for (Shape shape : children) {
+				UpdateContext updateContext = new UpdateContext(shape);
+				IUpdateFeature updateFeature = getFeatureProvider().getUpdateFeature(updateContext);
+				if (updateFeature != null && updateFeature.canUpdate(updateContext)) {
+					if (!checkUpdateNeeded || updateFeature.updateNeeded(updateContext).toBoolean()) {
+						connToUpdate.put(updateFeature, updateContext);
+					}
+				}
+			}
+		}
+		return connToUpdate;
 	}
 }
