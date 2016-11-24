fd7d5745a21f9644fb0d5296b32c5729b024f4c2
Aurelien Pupier <apupier@redhat.com>
2016-11-24 16:58:05 +0100

    Bug 508133 - Use FontRegistry
    It avoids to create/dispose similar Font each time the mouse hovers a
    new EditPart
    
    Change-Id: I5faad12396f672bd3355e7a94192003112283d7a
    Signed-off-by: Aurelien Pupier <apupier@redhat.com>

--- plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/contextbuttons/ContextButtonPad.java
+++ plugins/org.eclipse.graphiti.ui/src/org/eclipse/graphiti/ui/internal/contextbuttons/ContextButtonPad.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * <copyright>
  *
- * Copyright (c) 2005, 2013 SAP AG.
+ * Copyright (c) 2005, 2016 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -549,16 +549,6 @@ public class ContextButtonPad extends Shape implements ITransparencyProvider {
 		control.removeMouseTrackListener(mouseTrackListener);
 
 		super.removeNotify();
-
-		// Notify the ContextButton children of this pad to free its resources
-		// (holds a font). Introduced to fix bug 373298
-		List<?> childrenList = getChildren();
-		for (Object object : childrenList) {
-			if (object instanceof ContextButton) {
-				((ContextButton) object).dispose();
-			}
-
-		}
 	}
 
 	// ============================== painting ================================
